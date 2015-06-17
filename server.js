96183b373e5357d0997d3739865492b71e49a361
rahul Khengare <rahulk1306@gmail.com>
2015-06-17 18:48:14 +0530

    change in msg

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var http = require('http')
 var port = process.env.PORT || 443;
 http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
+    res.end('nodejs app CI demo in Azure \n');
     }).listen(port);
